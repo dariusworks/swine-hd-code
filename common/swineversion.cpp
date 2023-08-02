286747b86278e35d97c339eda571f6a77c2520d1
dw5 <29355584+dw5@users.noreply.github.com>
2023-08-02 16:40:43 +0300

    Update swineversion.cpp

--- common/swineversion.cpp
+++ common/swineversion.cpp
@@ -6,3 +6,19 @@ SVersion * __thiscall SVersion::SVersion(SVersion *this)
   this->VersionType = Gold;
   return this;
 }
+
+
+SwineVersionType __thiscall SVersion::GetVersionTypeFromString(SVersion *this,char *param_1)
+
+{
+  int version;
+  
+  version = _stricmp(param_1,"Developer");
+  if (version != 0) {
+    version = _stricmp(param_1,"HD Remaster");
+    if (version == 0) {
+      return Gold;
+    }
+  }
+  return Developer;
+}
